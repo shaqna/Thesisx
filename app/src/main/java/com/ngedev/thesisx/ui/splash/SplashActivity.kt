06f2b0a175fed58ec9991b6b140676d44c57e762
sxrxrx <85094525+sxrxrx@users.noreply.github.com>
2022-07-07 15:04:23 +0700

    modified

--- app/src/main/java/com/ngedev/thesisx/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/ngedev/thesisx/ui/splash/SplashActivity.kt
@@ -5,22 +5,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.ngedev.thesisx.MainActivity
 
 import com.ngedev.thesisx.R
 import com.ngedev.thesisx.ui.welcome.WelcomeActivity
 
 
 @SuppressLint("CustomSplashScreen")
-@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            observeUID()
+        }, 3000L)
+    }
+
+    private fun observeUID() {
+        if(isUserAlreadyHere()) {
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java)).also {
+                finish()
+            }
+        } else {
             startActivity(Intent(this@SplashActivity, WelcomeActivity::class.java)).also {
                 finish()
             }
-        }, 3000L)
+        }
+    }
+
+
+    private fun isUserAlreadyHere(): Boolean {
+        val auth = Firebase.auth
+        val uid = auth.currentUser?.uid
+        if (uid != null) {
+            return true
+        }
+        return false
     }
 }
